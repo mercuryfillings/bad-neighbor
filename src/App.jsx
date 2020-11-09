733a380dd1f30a7cf6f3de256f3c62243a544a6d
Scott Delbango <scott.delbango@gmail.com>
2020-11-09 16:25:34 -0500

    Initial rendering achieved

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Main from './Components/Main/Main'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import axios from 'axios'
+
+export default function Main() {
+  return (
+    <div>
+      <h1>Your neighbor sucks!</h1>
+    </div>
+  )
+}
