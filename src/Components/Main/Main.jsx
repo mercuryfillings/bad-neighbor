eab4b4259fbcdb4e467039ceb04f3eab8fd59034
Scott Delbango <scott.delbango@gmail.com>
2020-11-10 00:18:55 -0500

    We got data, we got data displaying, now let's see what we can do with it

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,19 +4,19 @@ import axios from 'axios'
 
 export default function Main() {
   const [query, setQuery] = useState('')
-  const [address, setAddress] = useState('')
   const [data, setData] = useState({})
   const BASE_URL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json/"
-  const ADDRESS = "?incident_address="
-
-  useEffect(async () => {
+  const ADDRESS_FILTER = "?incident_address="
+  const ERROR_MESSAGE = 'No data for this address'
+  const apiCall = async () => {
     try {
-      const response = await axios.get(BASE_URL + ADDRESS + '218 HIMROD STREET')
-      setData(response)
+      const response = await axios.get(BASE_URL + ADDRESS_FILTER + query.toUpperCase())
+      setData(response.data)
+      console.log(response)
     } catch (error) {
       console.log(error)
     }
-  }, [])
+  }
 
   const handleChange = (e) => {
     setQuery(e.target.value)
@@ -24,14 +24,15 @@ export default function Main() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setAddress(query)
+    apiCall()
   }
-  console.log(data)
+
 
   return (
     <div>
       <h1>Your neighbor sucks!</h1>
-      <h2>{address}</h2>
+      <h2>{data.length < 1 ? ERROR_MESSAGE : ''}</h2>
+      <h3>{data[0] ? data[0].agency : ''}</h3>
       <form onSubmit={handleSubmit}>
         <label>Enter your address: </label>
         <input onChange={handleChange} />
--- src/Components/api-helper.js
+++ src/Components/api-helper.js
@@ -1,10 +1,12 @@
-const BASE_URL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json/?incident_address="
 const axios = require('axios');
+const BASE_URL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json/"
+const ADDRESS_FILTER = "?incident_address="
 
 const apiCall = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}218 HIMROD STREET`)
-    console.log(response.data[0])
+    const response = await axios.get(BASE_URL + ADDRESS_FILTER + query.toUpperCase())
+    setData(response.data)
+    console.log(response)
   } catch (error) {
     console.log(error)
   }
@@ -20,11 +22,31 @@ export default apiCall
   // }, [])
   // original try below
   // const axios = require('axios');
-  // const apiCall = async () => {
+  
+  //const apiCall = async () => {
   //   try {
-  //     const response = await axios.get(`${BASE_URL}218 HIMROD STREET`)
-  //     console.log(response.data[0])
+  //     const response = await axios.get(BASE_URL + ADDRESS_FILTER + '218 HIMROD STREET')
+  //     setData(response)
+  //     console.log(response)
   //   } catch (error) {
   //     console.log(error)
   //   }
   // }
+
+  //useEffect(async () => {
+  //   try {
+  //     const response = await axios.get(BASE_URL + ADDRESS + '218 HIMROD STREET')
+  //     setData(response)
+  //   } catch (error) {
+  //       console.log(error)
+  //   }
+  // }, [])
+
+  //const apiCall = async () => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}218 HIMROD STREET`)
+//     console.log(response.data[0])
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
