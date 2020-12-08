7116db7d04562a6c7ffb26cab0e3b9de9eb55670
Scott Delbango <scott.delbango@gmail.com>
2020-12-08 17:50:10 -0500

    Styling fixes

--- src/Components/Responses/UsedTo.jsx
+++ src/Components/Responses/UsedTo.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Responses.css'
 
-export default function UsedTo() {
+export default function UsedTo(props) {
   return (
+    <>
     <div className='circle-u'>
       <h1 className='text'>There Was at One Point!</h1>
-    </div>
+      </div>
+      <section className='info-box'>
+        <p className='info'>Your building used to be Karen-positive, but it looks like whoever it was chilled out or moved away or died or something. There have been <span className='no-text'>{props.complaints}</span> frivolous 311 complaints at your address over the last <span className='no-text'>{props.time}</span> years, but that's gone way down recently.</p>
+      </section>
+    </>
   )
 }
