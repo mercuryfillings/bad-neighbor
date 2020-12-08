ef6545d146afc2acadc67d6b9f5f5d50f62193c7
Scott Delbango <scott.delbango@gmail.com>
2020-12-07 22:28:15 -0500

    Some cleanup

--- src/Components/Main/TODO.jsx
+++ src/Components/Main/TODO.jsx
@@ -1,8 +1,11 @@
 //TODO
 
 
-//need to clear old data
-//need to differentiate between bad address error and no bad neighbors (keep full data)
-//
+//clear old data
+//differentiate between bad address error and no bad neighbors (keep full data)
+//fix address input: remove st,nd,rd,th from numbers, convert st, rd, blvd, ave, remove .
+//add animations
+//add hover states to dots
+//styling styling styling
 
 //Issues
