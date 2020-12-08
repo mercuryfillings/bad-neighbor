86eeb109b090a5940ece485875f107e872f32bf3
Scott Delbango <scott.delbango@gmail.com>
2020-12-08 00:08:04 -0500

    More styling and added some copy

--- src/Components/Responses/No.jsx
+++ src/Components/Responses/No.jsx
@@ -3,8 +3,13 @@ import './Responses.css'
 
 export default function No() {
   return (
-    <div className='circle-n'>
-      <h1>No</h1>
-    </div>
+    <>
+      <div className='circle-n'>
+        <h1>No</h1>
+      </div>
+      <section className='info-box'>
+        <p className='info'>It seems like your neighbors are okay. Maybe it's your fault they called the cops on you.</p>
+      </section>
+    </>
   )
 }
--- src/Components/Responses/Yes.jsx
+++ src/Components/Responses/Yes.jsx
@@ -3,8 +3,13 @@ import './Responses.css'
 
 export default function Yes() {
   return (
-    <div className='circle-y'>
-      <h1>Yes</h1>
-    </div>
+    <>
+      <div className='circle-y'>
+        <h1>Yes</h1>
+      </div>
+      <section className='info-box'>
+        <p className='info'>There is ABSOLUTELY a Karen in your building. Don't look now, but I think I hear the cops outside.</p>
+      </section>
+    </>
   )
 }
